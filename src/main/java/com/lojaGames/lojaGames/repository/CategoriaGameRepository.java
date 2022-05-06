6399a8d8e9e459a49414baa3c610021ecaaa0eeb
Gabriela Felix <gabriela_2008pe@hotmail.com>
2022-05-05 22:36:55 -0300

    Primeiro commit

---
+++ src/main/java/com/lojaGames/lojaGames/repository/CategoriaGameRepository.java
@@ -0,0 +1,21 @@
+package com.lojaGames.lojaGames.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.lojaGames.lojaGames.model.CategoriaGame;
+
+public interface CategoriaGameRepository {
+
+	
+
+	@Repository
+		public interface ProdutoGameRepository extends JpaRepository<CategoriaGame, Long>{
+			public List<CategoriaGame> findAllByDescricaoContainingIgnoreCase (@Param("descricao")String descricao);
+		}
+
+
+}
---
+++ src/test/java/com/lojaGames/lojaGames/LojaGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojaGames.lojaGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
